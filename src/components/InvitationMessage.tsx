3e1ae4bc3287580db352172191e7c74069e002d0
sy.kang <sy.kang@lydianlabs.kr>
2026-06-01 12:20:37 +0900

    update

--- src/components/InvitationMessage.tsx
+++ src/components/InvitationMessage.tsx
@@ -3,7 +3,7 @@ import { Section } from './Section'
 
 export function InvitationMessage() {
   return (
-    <Section title="초대합니다">
+    <Section title="초대합니다!">
       <p className="invitation-message">{weddingConfig.message}</p>
     </Section>
   )
